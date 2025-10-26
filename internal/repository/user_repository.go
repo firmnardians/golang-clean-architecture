07ec9e02d7c760483f1e2345550b4085fe0662ae
firmnardians <firmnardians@gmail.com>
2025-10-26 16:15:55 +0700

    feat(user): add DeleteUser method to repository interface
    Implement user deletion functionality by ID. The method converts string ID to integer, finds the user, and removes them from the repository data. Returns USER_NOT_FOUND error if user doesn't exist.

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -1,10 +1,16 @@
 package repository
 
-import "fiber/internal/entity"
+import (
+	"errors"
+	"strconv"
+
+	"fiber/internal/entity"
+)
 
 type UserRepository interface {
 	GetAll() ([]entity.User, error)
 	Create(user entity.User) error
+	DeleteUser(key string) error
 }
 
 type userRepository struct {
@@ -25,5 +31,37 @@ func (r *userRepository) Create(user entity.User) error {
 	user.ID = len(r.data) + 1
 
 	r.data = append(r.data, user)
+	return nil
+}
+
+func (r *userRepository) DeleteUser(key string) error {
+	num, err := strconv.Atoi(key)
+	if err != nil {
+		return nil
+	}
+
+	users := r.data
+	keyInt := num
+
+	var found *entity.User
+	for _, u := range users {
+		if u.ID == keyInt {
+			found = &u
+			break
+		}
+	}
+
+	if found == nil {
+		return errors.New("USER_NOT_FOUND")
+	}
+
+	dataUsers := []entity.User{}
+	for _, u := range users {
+		if u.ID != keyInt {
+			dataUsers = append(dataUsers, u)
+		}
+	}
+
+	r.data = dataUsers
 	return nil
 }
